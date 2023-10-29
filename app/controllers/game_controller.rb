1f2fee51045d43103b8acf4427b45217c20c8b35
fdicera <67300276+fdicera@users.noreply.github.com>
2023-10-29 03:10:23 +0000

    Minimized code using dynamic paths and completed.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,6 @@
 class GameController < ApplicationController
 	# 2/6
-  def user_two_six
+  def user_play
     @number_of_dice = params.fetch("zebra").to_i
     @die_nums = params.fetch("dog").to_i
   
@@ -12,58 +12,7 @@ class GameController < ApplicationController
       @rolls.push(die)
     end
 
-    render({ :template => "game_templates/play_two_six"})
-
-  end
-
-  # 2/10
-  def user_two_ten
-    @number_of_dice = params.fetch("zebra").to_i
-    @die_nums = params.fetch("dog").to_i
-  
-    @rolls = []
-  
-    @number_of_dice.times do
-      die = rand(1..@die_nums)
-  
-      @rolls.push(die)
-    end
-
-    render({ :template => "game_templates/play_two_ten"})
-
-  end
-
-  # 1/20
-  def user_one_twenty
-    @number_of_dice = params.fetch("zebra").to_i
-    @die_nums = params.fetch("dog").to_i
-  
-    @rolls = []
-  
-    @number_of_dice.times do
-      die = rand(1..@die_nums)
-  
-      @rolls.push(die)
-    end
-
-    render({ :template => "game_templates/play_one_twenty"})
-
-  end
-
-  # 5/4
-  def user_five_four
-    @number_of_dice = params.fetch("zebra").to_i
-    @die_nums = params.fetch("dog").to_i
-  
-    @rolls = []
-  
-    @number_of_dice.times do
-      die = rand(1..@die_nums)
-  
-      @rolls.push(die)
-    end
-
-    render({ :template => "game_templates/play_five_four"})
+    render({ :template => "game_templates/random_play"})
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,7 @@ Rails.application.routes.draw do
 get("/", { :controller => "pages", :action => "zebra"})
 
 
- # 2/6
- get("/dice/:zebra/:dog", { :controller => "game", :action => "user_two_six"})
-
-
- # 2/10
- get("/dice/:zebra/:dog", { :controller => "game", :action => "user_two_ten"})
-
-
- # 1/20
- get("/dice/:zebra/:dog", { :controller => "game", :action => "user_one_twenty"})
-
-
- # 5/4
- get("/dice/:zebra/:dog", { :controller => "game", :action => "user_five_four"})
+ # flexible
+ get("/dice/:zebra/:dog", { :controller => "game", :action => "user_play"})
 
 end
