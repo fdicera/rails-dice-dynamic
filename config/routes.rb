06bb71041d77eee72247ee3d18b53a7c236413c6
fdicera <67300276+fdicera@users.noreply.github.com>
2023-10-29 00:09:57 +0000

    Added all dice pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
 
-  # home
-  get("/", { :controller => "pages", :action => "zebra"})
+# home
+get("/", { :controller => "pages", :action => "zebra"})
 
 
  # 2/6
- get("/dice/2/6", { :controller => "game", :action => "user_two_six"})
+ get("/dice/:zebra/:dog", { :controller => "game", :action => "user_two_six"})
 
 
  # 2/10
- get("/dice/2/10", { :controller => "game", :action => "user_two_ten"})
+ get("/dice/:zebra/:dog", { :controller => "game", :action => "user_two_ten"})
 
 
  # 1/20
- get("/dice/1/20", { :controller => "game", :action => "user_one_twenty"})
+ get("/dice/:zebra/:dog", { :controller => "game", :action => "user_one_twenty"})
 
 
  # 5/4
- get("/dice/5/4", { :controller => "game", :action => "user_five_four"})
+ get("/dice/:zebra/:dog", { :controller => "game", :action => "user_five_four"})
+
 end
